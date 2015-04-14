70f57e21a1440006bcead4ad4713d7c0ba695dd3
Steve Jahns <s.t.jahns@gmail.com>
2015-04-13 20:48:07 -0600

    Moved 'leaks' source + assets to own repository

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to leaks
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/leaks/core_test.clj
@@ -0,0 +1,7 @@
+(ns leaks.core-test
+  (:require [clojure.test :refer :all]
+            [leaks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
